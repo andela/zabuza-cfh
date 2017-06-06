c9e8aa4e0967026647dd09a269c6f4d173379c62
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-06-07 00:45:19 +0100

    chore: setup database and eslint
    Database setup
    * Download and install mongodb
    * Connect to mongodb
    * Install dotenv as a dependency
    * Create a .env file to define environment variables
    * Call an instance of config in server.js and start the server
    
    Eslint setup
    * Install eslint, eslint-config-airbnb-base and eslint-plugin-import as dev dependencies
    * Create a .eslintrc file and add config rules
    
    Finishes #146623131

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var express = require('express'),
     logger = require('mean-logger'),
     io = require('socket.io');
 
+// dotenv.config();
+
+
 /**
  * Main application entry file.
  * Please note that the order of loading is important.
@@ -15,6 +18,7 @@ var express = require('express'),
 
 //Load configurations
 //if test env, load example file
+
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     config = require('./config/config'),
     auth = require('./config/middlewares/authorization'),
