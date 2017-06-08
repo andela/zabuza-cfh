29cd6540c95ab6ff0ebed78a137c06ca5cc7258f
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-06-08 03:50:55 +0100

    fix: merge conflict
    * ignore .env and package-lock.json
    
    Fixes #146623131

--- server.js
+++ server.js
@@ -8,17 +8,13 @@ var express = require('express'),
     logger = require('mean-logger'),
     io = require('socket.io');
 
-// dotenv.config();
-
-
 /**
  * Main application entry file.
  * Please note that the order of loading is important.
  */
 
 //Load configurations
-//if test env, load example file
-
+//if test env, load example file`
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     config = require('./config/config'),
     auth = require('./config/middlewares/authorization'),
