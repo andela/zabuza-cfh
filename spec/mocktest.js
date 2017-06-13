1506bae7093b19eeb232f978da9813f382e63dbe
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-06-13 15:00:49 +0100

    chore: setup unit testing for the front end
    * Install dependencies
    * Initialize karma.conf.js file and modify settings
    * Modify scripts in .travis.yml to install packages before running karma tests
    
    [Start #146623143]

---
+++ spec/mocktest.js
@@ -0,0 +1,11 @@
+describe('Dummy tests', () => {
+  it('should test that 1 + 2 equals 3', () => {
+    const n = 1 + 3;
+    expect(n).toBe(4);
+  });
+
+  it('should test that 5 * 3 equals 15', () => {
+    const f = 5 * 3;
+    expect(f).toBe(15);
+  });
+});
