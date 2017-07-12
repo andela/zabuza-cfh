ee31ad28b1475296a072580df1d6c4617d637f84
DanielAmah <amahdanieljack@gmail.com>
2017-07-12 13:14:25 +0100

    refactor code

--- public/js/spinner.js
+++ public/js/spinner.js
@@ -8,6 +8,8 @@
         */
        const galleryspin = (sign) => {
          spinner = document.querySelector('#spinner');
-         if (!sign) { angle -= 45; } else { angle += 45; }
-         spinner.setAttribute('style', `-webkit-transform: rotateY(${angle}deg); -moz-transform: rotateY(${angle}deg); transform: rotateY(${angle }deg);`);
+         if (!sign) {
+           angle -= 45;
+         } else { angle += 45; }
+         spinner.setAttribute('style', `-webkit-transform: rotateY(${angle}deg); -moz-transform: rotateY(${angle}deg); transform: rotateY(${angle}deg);`);
        };
