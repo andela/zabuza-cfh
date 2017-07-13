c71869c25c5dad75eed4d082d72ff3c9cfb54142
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2017-07-13 11:59:19 +0100

    feature:  Users should receive a JWT on successful signup
        * create a middle to for authorization validate jwt for each route in authorization.js
        * create signUp function to handle ng-click event when signup button is clicked
    
        FINISHES [146623151]

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,10 @@
-var path = require('path'),
-rootPath = path.normalize(__dirname + '/../..');
+var path = require('path');
+var	rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
 
 module.exports = {
 	root: rootPath,
 	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
+	db: process.env.MONGOHQ_URL,
+	secret: process.env.JWT_SECRET
 };
--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -1,23 +1,24 @@
+/* global jwt secret */
 /**
  * Generic require login routing middleware
  */
-exports.requiresLogin = function(req, res, next) {
-    if (!req.isAuthenticated()) {
-        return res.send(401, 'User is not authorized');
-    }
-    next();
+exports.requiresLogin = function (req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.send(401, 'User is not authorized');
+  }
+  next();
 };
 
 /**
  * User authorizations routing middleware
  */
 exports.user = {
-    hasAuthorization: function(req, res, next) {
-        if (req.profile.id != req.user.id) {
-            return res.send(401, 'User is not authorized');
-        }
-        next();
+  hasAuthorization: function (req, res, next) {
+    if (req.profile.id != req.user.id) {
+      return res.send(401, 'User is not authorized');
     }
+    next();
+  }
 };
 
 /**
@@ -31,3 +32,29 @@ exports.user = {
 //         next();
 //     }
 // };
+
+// Routing process of the middleware to verify a user token
+exports.checkToken = (req, res, next) => {
+  // checking header or url parameters or post parameters for token
+  if (req.url.startsWith('/auth')) return next();
+  const token = req.cookies.token;
+  // decoding the token
+  if (token) {
+    // verifies secret and checks
+    jwt.verify(token, secret, function (error, decoded) {
+      if (error) {
+        return res.status(403).json({
+          message: 'Failed to authenticate token.'
+        });
+      }   // if the authentication process was succesful, save to request for use in other routes
+      req.decoded = decoded;
+      next();
+    });
+  } else {
+    // if there is no token available
+    // return an error
+    return res.status(200).send({
+      message: 'No token returned.'
+    });
+  }
+};
