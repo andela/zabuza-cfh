9906553b97e0cc606bf189fdf7333fb59f0dec53
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-07-15 14:37:18 +0100

    chore: Setup front-end unit testing * Install the required dependencies * Setup karma.conf.js with the required configuration settings

---
+++ spec/mocktest.js
@@ -0,0 +1,12 @@
+/* global expect */
+describe('Dummy tests', () => {
+  it('should test that 1 + 2 equals 3', () => {
+    const n = 1 + 3;
+    expect(n).toBe(4);
+  });
+
+  it('should test that 5 * 3 equals 15', () => {
+    const f = 5 * 3;
+    expect(f).toBe(15);
+  });
+});
